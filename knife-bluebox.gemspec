398dd0012fe7fb4d62bfdb086d48dfb4ff0a6a71
Christian Paredes <cp@redbluemagenta.com>
2011-04-06 15:54:04 -0700

    Fixing a few bugs.

--- knife-bluebox.gemspec
+++ knife-bluebox.gemspec
@@ -13,8 +13,8 @@ Gem::Specification.new do |s|
   s.email = "adam@opscode.com"
   s.homepage = "http://wiki.opscode.com/display/chef"
 
-  s.add_dependency "chef", ">= 0.9.14"
-  s.add_dependency "fog", "~> 0.6.0"
+  s.add_dependency "chef", "~> 0.10.0"
+  s.add_dependency "fog", "~> 0.7.0"
   s.require_path = 'lib'
   s.files = %w(LICENSE README.rdoc) + Dir.glob("lib/**/*")
 end
--- lib/chef/knife/bluebox_server_create.rb
+++ lib/chef/knife/bluebox_server_create.rb
@@ -33,6 +33,11 @@ class Chef
         :description => "The flavor of server",
         :default => "94fd37a7-2606-47f7-84d5-9000deda52ae"
 
+        option :chef_node_name,
+        :short => "-N NAME",
+        :long => "--node-name NAME",
+        :description => "The Chef node name for your new node"
+
       option :image,
         :short => "-i IMAGE",
         :long => "--image IMAGE",
@@ -56,6 +61,22 @@ class Chef
         :description => "Disables the bootstrapping process.",
         :default => true
 
+      option :distro,
+        :short => "-d DISTRO",
+        :long => "--distro DISTRO",
+        :description => "Bootstrap a distro using a template",
+        :default => "ubuntu10.04-gems"
+
+      option :identity_file,
+        :short => "-I IDENTITY_FILE",
+        :long => "--identity-file IDENTITY_FILE",
+        :description => "The SSH identity file used for authentication"
+
+      option :load_balancer,
+        :short => "-b LB",
+        :long => "--load_balancer LB",
+        :description => "Adds server to the specified load balanced application."
+
       def h
         @highline ||= HighLine.new
       end
@@ -67,12 +88,13 @@ class Chef
         require 'readline'
         require 'erb'
 
-        bluebox = Fog::Bluebox::Compute.new(
+        bluebox = Fog::Compute.new(
+          :provider => 'Bluebox',
           :bluebox_customer_id => Chef::Config[:knife][:bluebox_customer_id],
           :bluebox_api_key => Chef::Config[:knife][:bluebox_api_key]
         )
 
-         flavors = bluebox.flavors.inject({}) { |h,f| h[f.id] = f.description; h }
+        flavors = bluebox.flavors.inject({}) { |h,f| h[f.id] = f.description; h }
         images  = bluebox.images.inject({}) { |h,i| h[i.id] = i.description; h }
 
         puts "#{h.color("Deploying a new Blue Box Block...", :green)}\n\n"
@@ -80,7 +102,8 @@ class Chef
           :flavor_id => config[:flavor],
           :image_id => config[:image],
           :user => config[:username],
-          :password => config[:password]
+          :password => config[:password],
+          :lb_applications => config[:load_balancer]
           }
         server_args[:ssh_key] = Chef::Config[:knife][:ssh_key] if Chef::Config[:knife][:ssh_key]
 
@@ -92,7 +115,7 @@ class Chef
         begin
 
           # Make sure we could properly queue the server for creation on BBG.
-          raise Fog::Bluebox::BlockInstantiationError if server.status != "queued"
+          raise Fog::Bluebox::Compute::BlockInstantiationError if server.status != "queued"
           puts "#{h.color("Hostname", :cyan)}: #{server.hostname}"
           puts "#{h.color("Server Status", :cyan)}: #{server.status.capitalize}"
           puts "#{h.color("Flavor", :cyan)}: #{flavors[server.flavor_id]}"
@@ -102,13 +125,13 @@ class Chef
           # The server was succesfully queued... Now wait for it to spin up...
           print "\n#{h.color("Requesting status of #{server.hostname}\n", :magenta)}"
 
-          # Allow for 5 minutes to time out...
+          # Allow for 10 minutes to time out...
           # ready? will raise Fog::Bluebox::BlockInstantiationError if block creation fails.
-          unless server.wait_for( 5 * 60 ){ print "."; STDOUT.flush; ready? }
+          unless server.wait_for( 10 * 60 ){ print "."; STDOUT.flush; ready? }
 
-            # The server wasn't started in 5 minutes... Send a destroy call to make sure it doesn't spin up on us later...
+            # The server wasn't started in 10 minutes... Send a destroy call to make sure it doesn't spin up on us later...
             server.destroy
-            raise Fog::Bluebox::BlockInstantiationError, "BBG server not available after 5 minutes"
+            raise Fog::Bluebox::Compute::BlockInstantiationError, "BBG server not available after 5 minutes"
 
           else
             print "\n\n#{h.color("BBG Server startup succesful.  Accessible at #{server.hostname}\n", :green)}"
@@ -134,6 +157,7 @@ class Chef
               bootstrap.config[:identity_file] = config[:identity_file]
               bootstrap.config[:chef_node_name] = config[:chef_node_name] || server.hostname
               bootstrap.config[:use_sudo] = true
+              bootstrap.config[:distro] = config[:distro]
               bootstrap.run
             rescue Errno::ECONNREFUSED
               puts h.color("Connection refused on SSH, retrying - CTRL-C to abort")
@@ -147,7 +171,7 @@ class Chef
 
           end
 
-        rescue Fog::Bluebox::BlockInstantiationError => e
+        rescue Fog::Bluebox::Compute::BlockInstantiationError => e
 
           puts "\n\n#{h.color("Encountered error starting up BBG block. Auto destroy called.  Please try again.", :red)}"
 
